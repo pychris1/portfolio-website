0d77ae493d56fe540b6da08e62b864b8766456ea
James Robertson <140358457+pychris1@users.noreply.github.com>
2024-11-16 19:53:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
 
 menuIcon.onclick = () => {
-    navLinks.classList.toggles('active');
+    navLinks.classList.toggle('active');
 }
